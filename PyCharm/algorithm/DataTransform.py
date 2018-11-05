241a66a487fe0a65bf832d11cf63f0d4ca8c333f
owuordickson <owuordickson@gmail.com>
2018-11-05 12:21:22 +0100

    tests dataset, and calculates time difference for every step

--- PyCharm/algorithm/DataTransform.py
+++ PyCharm/algorithm/DataTransform.py
@@ -11,15 +11,17 @@ This class prepares a dataset to be transformed by any step
 """
 
 import csv
-from PyCharm.algorithm.TimeLag import test_time
 import numpy as np
+from datetime import datetime
+import time
 
-class DataSet:
+
+class DataTransform:
 
     def __init__(self,filename):
 
         #1. Test dataset
-        ok,data = self.test_dataset(filename)
+        ok,data = DataTransform.test_dataset(filename)
 
         if ok:
             print("Dataset Ok")
@@ -47,46 +49,57 @@ class DataSet:
                 item = self.data[i][c + 1]  # because time is the first column in dataset (it is ignored)
                 multi_data[c].append(item)
 
-        # print(multi_dataset)
+        #print(multi_data)
         return multi_data
 
     def transform_data(self,ref_column, step):
         #NB: Restructure dataset based on reference item
+        if self.time_ok:
+            #1. Calculate time difference using step
+            ok, time_diffs = self.get_time_diffs(step)
+            if ok == False:
+                msg = "Error: Time in row " + str(time_diffs[0]) + " or row " + str(time_diffs[1]) + " is not valid."
+                raise Exception(msg)
+                #return msg
+            else:
+                #1. Load all the titles
+                first_row = self.data[0]
 
-        #1. Load all the titles
-        first_row = self.data[0]
+                #2. Creating titles without time column
+                no_columns = (len(first_row) - 1)
+                title_row = [None] * no_columns
+                for c in range(no_columns):
+                    title_row[c] = first_row[c + 1]
 
-        #2. Creating titles without time column
-        no_columns = (len(first_row) - 1)
-        title_row = [None] * no_columns
-        for c in range(no_columns):
-            title_row[c] = first_row[c + 1]
+                ref_name = str(title_row[ref_column])
+                title_row[ref_column] = ref_name + "**"
+                new_dataset = [title_row]
 
-        ref_name = str(title_row[ref_column])
-        title_row[ref_column] = ref_name + "**"
-        new_dataset = [title_row]
+                #3. Split the original dataset into gradual items
+                gradual_items = self.multi_data
 
-        #3. Split the original dataset into gradual items
-        gradual_items = self.multi_data
+                #4. Transform the data using (row) n+step
+                for j in range(len(self.data)):
+                    # time_diff = 0
+                    ref_item = gradual_items[ref_column]
 
-        #4. Transform the data using (row) n+step
-        for j in range(len(self.data)):
-            # time_diff = 0
-            ref_item = gradual_items[ref_column]
+                    if j < len(ref_item) - step:
+                        init_array = [ref_item[j]]
 
-            if j < len(ref_item) - step:
-                init_array = [ref_item[j]]
-
-                for i in range(len(gradual_items)):
-                    if i < len(gradual_items) and i != ref_column:
-                        gradual_item = gradual_items[i];
-                        temp = [gradual_item[j + step]]
-                        temp_array = np.append(init_array, temp, axis=0)
-                        init_array = temp_array
-                new_dataset.append(list(init_array))
-                #return new_dataset
-        #print(new_dataset)
-        return new_dataset;
+                        for i in range(len(gradual_items)):
+                            if i < len(gradual_items) and i != ref_column:
+                                gradual_item = gradual_items[i];
+                                temp = [gradual_item[j + step]]
+                                temp_array = np.append(init_array, temp, axis=0)
+                                init_array = temp_array
+                        new_dataset.append(list(init_array))
+                        #return new_dataset
+                #print(new_dataset)
+                return new_dataset,time_diffs;
+        else:
+            msg = "Fatal Error: Time format in 1st column could not be processed"
+            raise Exception(msg)
+            #return msg
 
     def get_representativity(self, step):
 
@@ -118,8 +131,29 @@ class DataSet:
             else:
                 return 0
 
+    def get_time_diffs(self, step):
 
-    def test_dataset(self,filename):
+        time_diffs = []
+
+        for i in range(1, len(self.data)):
+            if i < (len(self.data) - step):
+                temp_1 = self.data[i][0]
+                temp_2 = self.data[i + step][0]
+
+                stamp_1 = DataTransform.get_timestamp(temp_1)
+                stamp_2 = DataTransform.get_timestamp(temp_2)
+
+                if stamp_1 == False or stamp_2 == False:
+                    return False, [i + 1, i + step + 1]
+
+                time_diff = (stamp_2 - stamp_1)
+                time_diffs.append(time_diff)
+
+        # print(time_lags)
+        return True, time_diffs
+
+    @staticmethod
+    def test_dataset(filename):
         #NB: test the dataset attributes: time|item_1|item_2|...|item_n
         #return true and (list)dataset if it is ok
 
@@ -134,8 +168,29 @@ class DataSet:
 
         #3. check if the retrieved time is valid
         try:
-            time_ok,t_stamp = test_time(raw_time)
+            time_ok,t_stamp = DataTransform.test_time(raw_time)
         except ValueError:
             return False,temp
         else:
             return time_ok,temp
+
+    @staticmethod
+    def get_timestamp(time_data):
+        try:
+            ok, stamp = DataTransform.test_time(time_data)
+        except ValueError:
+            return False
+        else:
+            return stamp
+
+    @staticmethod
+    def test_time(time_data):
+        # add all the possible formats
+        time_formats = ('%Y-%m-%d', '%d.%m.%Y', '%d/%m/%Y', "%H:%M:%S")
+
+        for fmt in time_formats:
+            try:
+                return True, time.mktime(datetime.strptime(time_data, fmt).timetuple())
+            except ValueError:
+                pass
+        raise ValueError('no valid date format found')
