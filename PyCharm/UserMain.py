4921d31b2ee901cc7f860ba8c301bdfc99d3058c
owuordickson <owuordickson@gmail.com>
2018-11-13 15:31:25 +0100

    improved printing task for patterns

--- PyCharm/UserMain.py
+++ PyCharm/UserMain.py
@@ -27,11 +27,12 @@ def algorithm_init(filename,ref_item,minsup,minrep):
         #print("Transformation Step (max): "+str(step))
 
         # TRANSFORM DATA (for each step)
+        patterns = 0
         for s in range(max_step):
             step = s+1 # because for-loop is not inclusive from range: 0 - max_step
             # 3. Calculate representativity
             chk_rep,rep_info = dataset.get_representativity(step)
-            print(rep_info)
+            #print(rep_info)
 
             if chk_rep:
                 # 4. Transform data
@@ -39,31 +40,52 @@ def algorithm_init(filename,ref_item,minsup,minrep):
                 #print(data)
 
                 # 5. Execute GRAANK for each transformation
-                D1, S1, T1 = Graank(Trad(list(data)), minsup, time_diffs, eq=False)
+                title, D1, S1, T1 = Graank(Trad(list(data)), minsup, time_diffs, eq=False)
 
-                print('Pattern : Support')
-                pr = 0
-                for i in range(len(D1)):
-                    # D is the Gradual Patterns, S is the support for D and T is time lag
-                    if (str(ref_item+1)+'+' in D1[i]) or (str(ref_item+1)+'-' in D1[i]):
-                        # select only relevant patterns w.r.t *reference item
-                        print(str(D1[i]) + ' : ' + str(S1[i]) + ' | ' + str(T1[i]))
-                        pr = pr + 1
-                if pr > 0:
+                pattern_found = check_for_pattern(ref_item,D1)
+                if pattern_found == True:
+                    print(rep_info)
+                    for line in title:
+                        print(line)
+                    print('Pattern : Support')
+                    for i in range(len(D1)):
+                        # D is the Gradual Patterns, S is the support for D and T is time lag
+                        if (str(ref_item+1)+'+' in D1[i]) or (str(ref_item+1)+'-' in D1[i]):
+                            # select only relevant patterns w.r.t *reference item
+                            print(str(D1[i]) + ' : ' + str(S1[i]) + ' | ' + str(T1[i]))
+                            patterns = patterns + 1
                     print("---------------------------------------------------------")
-                else:
-                    print("Oops! no relevant pattern was found")
-                    print("---------------------------------------------------------")
+
+        if patterns == 0:
+            print("Oops! no relevant pattern was found")
+            print("---------------------------------------------------------")
 
     except Exception as error:
         print(error)
+
+
+def check_for_pattern(ref_item, R):
+    pr = 0
+    for i in range(len(R)):
+        # D is the Gradual Patterns, S is the support for D and T is time lag
+        if (str(ref_item + 1) + '+' in R[i]) or (str(ref_item + 1) + '-' in R[i]):
+            # select only relevant patterns w.r.t *reference item
+            pr = pr + 1
+    if pr > 0:
+        return True
+    else:
+        return False
 
 
 def main(filename,ref_item,minsup,minrep):
     algorithm_init(filename,ref_item,minsup,minrep)
 
 
-#main("data/test.csv",0,0.1,0.6)
+import time
+start_time = time.time()
+#main("data/test.csv",0,0.1,0.5)
+main("data/rain_temp2013-2015_full.csv",0,0.5,0.5)
 #main("data/ndvi_test.csv",0,0.5,0.8)
 #main("data/ndvi_kenya.csv", 2, 0.5, 0.8)
-main("data/ndvi_towns.csv", 0, 0.5, 0.5)
+#main("data/ndvi_towns_full.csv", 0, 0.5, 0.5)
+print("--- %s seconds ---" % (time.time() - start_time))
