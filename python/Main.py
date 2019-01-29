1362a827e2b9801cd016479f36878c60431ceae8
owuordickson <owuordickson@gmail.com>
2019-01-29 19:09:19 +0300

    updated python code

--- python/Main.py
+++ python/Main.py
@@ -2,7 +2,7 @@ from python.algorithm.UserMain import *
 
 import time
 start_time = time.time()
-main("data/test.csv", 0, 0.1, 0.98)
+main("data/test.csv", 0, 0.5, 0.5)
 #main("data/rain_temp2013-2015_full.csv",0,0.5,0.5)
 #main("data/ndvi_test.csv",0,0.5,0.8)
 #main("data/ndvi_kenya.csv", 2, 0.5, 0.8)
