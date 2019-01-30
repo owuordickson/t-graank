60e4190a71c30812173fec8cccaac52e0ef66336
owuordickson <owuordickson@gmail.com>
2019-01-30 11:27:15 +0300

    updated info

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-    ]
-    #test_suite='tests',
+    ],
+    test_suite='tests'
     #tests_require=test_requirements
 )
