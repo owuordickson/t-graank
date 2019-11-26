62e8a532e13d8764c137845b5c7ed3c7c171657a
owuordickson <owuordickson@gmail.com>
2019-11-26 11:38:10 +0100

    configuration

--- setup.py
+++ setup.py
@@ -12,10 +12,8 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    # TODO: put package requirements here
-    'scikit-fuzzy','numpy'
-]
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read()
 
 test_requirements = [
     # TODO: put package test requirements here
@@ -43,12 +41,7 @@ setup(
         'License :: OSI Approved :: Massachusetts Institute of Technology (MIT) License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     tests_require=test_requirements
