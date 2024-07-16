c9690e4b6c79b13e80037714efefc4886e191cae
owuordickson <owuordickson@gmail.com>
2024-07-16 10:10:04 +0300

    added configs module

--- src/TemporalGP/cli_main.py
+++ src/TemporalGP/cli_main.py
@@ -10,17 +10,32 @@ Description:
     r -> representativity
 """
 
-import sys
-from optparse import OptionParser
-from so4gp import write_file
+
+import so4gp as sgp
+from .configs.configs_loader import load_configs
 from .TGP.t_graank import TGrad
 
 
-def tgp_app(f_path, ref_item, min_sup, min_rep, num_cores, eq=False):
+def execute_tgp(f_path: str, min_sup: float, ref_col: int, min_rep: float, num_cores: int, allow_mp: bool, eq=False):
+    """
+    Executes T-GRAANK algorithm using the user-specified configuration options.
+
+    :param f_path: input path of a CSV file/Pandas DataFrame.
+    :param ref_col: reference column of the data-set.
+    :param min_sup: minimum support threshold.
+    :param min_rep: minimum representativity threshold.
+    :param num_cores: number of available cores.
+    :param allow_mp: allow multiprocessing.
+    :param eq: assign equal values as valid?
+    :return: results in string format.
+    """
     try:
 
-        tgp = TGrad(f_path, eq, min_sup, ref_item, min_rep, num_cores)
-        if num_cores >= 1:
+        if num_cores <= 1:
+            num_cores = sgp.get_num_cores()
+
+        tgp = TGrad(f_path, eq, min_sup, ref_col, min_rep, num_cores)
+        if allow_mp:
             msg_para = "True"
             list_tgp = tgp.discover_tgp(parallel=True)
         else:
@@ -38,7 +53,7 @@ def tgp_app(f_path, ref_item, min_sup, min_rep, num_cores, eq=False):
 
         for txt in tgp.titles:
             col = int(txt[0])
-            if col == ref_item:
+            if col == ref_col:
                 output_txt += (str(txt[0]) + '. ' + str(txt[1].decode()) + '**' + '\n')
             else:
                 output_txt += (str(txt[0]) + '. ' + str(txt[1].decode()) + '\n')
@@ -52,7 +67,7 @@ def tgp_app(f_path, ref_item, min_sup, min_rep, num_cores, eq=False):
                 for tgp in obj:
                     count += 1
                     output_txt += (str(tgp.to_string()) + ' : ' + str(tgp.support) +
-                                ' | ' + str(tgp.time_lag.to_string()) + '\n')
+                                   ' | ' + str(tgp.time_lag.to_string()) + '\n')
 
         output_txt += "\n\n Number of patterns: " + str(count) + '\n'
         return output_txt
@@ -63,62 +78,18 @@ def tgp_app(f_path, ref_item, min_sup, min_rep, num_cores, eq=False):
 
 
 def terminal_app():
-    if not sys.argv:
-        # pType = sys.argv[1]
-        file_path = sys.argv[1]
-        ref_col = sys.argv[2]
-        min_sup = sys.argv[3]
-        min_rep = sys.argv[4]
-        allow_p = sys.argv[5]
-    else:
-        optparser = OptionParser()
-        optparser.add_option('-f', '--inputFile',
-                             dest='file',
-                             help='path to file containing csv',
-                             # default=None,
-                             # default='../datasets/DATASET2.csv',
-                             default='../datasets/rain_temp2013-2015.csv',
-                             # default='../datasets/Directio.csv',
-                             type='string')
-        optparser.add_option('-c', '--refColumn',
-                             dest='refCol',
-                             help='reference column',
-                             default=1,
-                             type='int')
-        optparser.add_option('-s', '--minSupport',
-                             dest='minSup',
-                             help='minimum support value',
-                             default=0.5,
-                             type='float')
-        optparser.add_option('-r', '--minRepresentativity',
-                             dest='minRep',
-                             help='minimum representativity',
-                             default=0.5,
-                             type='float')
-        optparser.add_option('-p', '--allowMultiprocessing',
-                             dest='allowPara',
-                             help='allow multiprocessing',
-                             default=1,
-                             type='int')
-        (options, args) = optparser.parse_args()
-
-        if options.file is None:
-            print('No datasets-set filename specified, system with exit')
-            print("Usage: $python3 cli_main.py -f filename.csv -c refColumn -s minSup  -r minRep")
-            sys.exit('System will exit')
-
-        file_path = options.file
-        ref_col = options.refCol
-        min_sup = options.minSup
-        min_rep = options.minRep
-        allow_p = options.allowPara
-
+    """
+        Initializes and executes T-GRAANK algorithm.
+        :return:
+    """
     import time
     # import tracemalloc
 
+    cfg = load_configs()
+
     start = time.time()
     # tracemalloc.start()
-    res_text = tgp_app(file_path, ref_col, min_sup, min_rep, allow_p)
+    res_text = execute_tgp(cfg.file, cfg.minSup, cfg.refCol, cfg.minRep, cfg.numCores, cfg.allowPara)
     # snapshot = tracemalloc.take_snapshot()
     end = time.time()
 
@@ -126,5 +97,5 @@ def terminal_app():
     # wr_text += (Profile.get_quick_mem_use(snapshot) + "\n")
     wr_text += str(res_text)
     f_name = str('res_tgp' + str(end).replace('.', '', 1) + '.txt')
-    write_file(wr_text, f_name, True)
+    sgp.write_file(wr_text, f_name, True)
     print(wr_text)
---
+++ src/TemporalGP/configs/configs_loader.py
@@ -0,0 +1,92 @@
+# SPDX-License-Identifier: GNU GPL v3
+# This file is dual licensed under the terms of the GNU General Public, Version
+# 3.0.  See the LICENSE file in the root of this
+# repository for complete details.
+
+"""
+Loads default configurations from 'configs.ini' file
+"""
+
+import os
+import sys
+import configparser
+from optparse import OptionParser
+from ypstruct import struct
+
+
+def load_configs():
+    options_gp = struct()
+    options_tgp = struct()
+
+    # Load configuration from file
+    config = configparser.ConfigParser()
+    try:
+        # Get the directory of the current script
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        config_path = 'configs.ini'
+        config_file = os.path.join(script_dir, config_path)
+        config.read(config_file)
+
+        # 1. (configs.ini) GP Configurations
+        options_gp.file_path = str(config.get('gradual-patterns', 'file_path'))
+        options_gp.min_sup = float(config.get('gradual-patterns', 'min_support'))
+        options_gp.num_cores = int(config.get('computation', 'cpu_cores'))
+        options_gp.allow_multiprocessing = int(config.get('computation', 'allow_multiprocessing'))
+
+        # 2. (configs.ini) TGP Configurations
+        options_tgp.ref_col = int(config.get('temporal-gradual-patterns', 'ref_column'))
+        options_tgp.min_rep = float(config.get('temporal-gradual-patterns', 'min_representation'))
+    except configparser.NoSectionError:
+        # 1. (Default) GP Configurations
+        options_gp.file_path = ""
+        options_gp.min_sup = 0.5
+        options_gp.num_cores = 1
+        options_gp.allow_multiprocessing = 1
+
+        # 2. (Default) TGP Configurations
+        options_tgp.ref_col = 1
+        options_tgp.min_rep = 0.5
+
+    optparser = OptionParser()
+    optparser.add_option('-f', '--inputFile',
+                         dest='file',
+                         help='path to file containing csv',
+                         default=options_gp.file_path,
+                         type='string')
+    optparser.add_option('-s', '--minSupport',
+                         dest='minSup',
+                         help='minimum support value',
+                         default=options_gp.min_sup,
+                         type='float')
+    optparser.add_option('-p', '--allowMultiprocessing',
+                         dest='allowPara',
+                         help='allow multiprocessing',
+                         default=options_gp.allow_multiprocessing,
+                         type='int')
+    optparser.add_option('-n', '--cores',
+                         dest='numCores',
+                         help='number of cores',
+                         default=options_gp.num_cores,
+                         type='int')
+    optparser.add_option('-c', '--refColumn',
+                         dest='refCol',
+                         help='reference column',
+                         default=options_tgp.ref_col,
+                         type='int')
+    optparser.add_option('-r', '--minRepresentativity',
+                         dest='minRep',
+                         help='minimum representativity',
+                         default=options_tgp.min_rep,
+                         type='float')
+    (options, args) = optparser.parse_args()
+
+    if (options.file is None) or options.file == '':
+        print('No datasets-set filename specified, system with exit')
+        print("Usage: $python3 cli_main.py -f filename.csv -c refColumn -s minSup  -r minRep")
+        sys.exit('System will exit')
+
+    # configs_data = {
+    #    "gp_options": options_gp,
+    #    "tgp_options": options_tgp,
+    # }
+    return options
