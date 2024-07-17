1c11a2c44b6af6e56142a6a7c6cdf77eda29ea33
owuordickson <owuordickson@gmail.com>
2024-07-17 22:26:58 +0300

    added

--- src/__test__.py
+++ src/__test__.py
@@ -11,6 +11,7 @@ tgp.discover_tgp()
 squared_diff = (tgp.mi_arr - tgp.initial_mutual_info) ** 2
 absolute_error = np.sqrt(squared_diff)
 
-# print(f"Diff: {squared_diff}\n")
+# print(f"Initial MI: {tgp.initial_mutual_info}\n")
+# print(f"Delayed MIs: {tgp.mi_arr}\n")
 # print(f"Abs.E.: {absolute_error}\n")
 print(f"Optimal Step: {np.argmin(absolute_error, axis=0)}\n")
